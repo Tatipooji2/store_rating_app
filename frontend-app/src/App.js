44efbf82bd5648fe64bf2002e4673b87441a724e
LAKSHMI POOJITHA <2100030538@kluniversity.in>
2025-03-17 16:29:31 +0530

    commit

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Login from './components/login';
 import Register from './components/register';
 import Navbar from './components/navbar';
+import Admin from './components/admin';
+import User from './components/user';
+import Owner from './components/owner';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
           <Routes>
+          <Route path="/owner" element={<Owner />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Admin/>}/>
+            <Route path="/user" element={<User/>}/>
             <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Navbar></Navbar>}/>
+            <Route path="/" element={<Navbar/>}/>
             </Routes>
-        </header>
-      </div>
     </Router>
   );
 }
